7cb584b75950c187c213d8cbc2853042aef1a7da
AJ West <ajwest@gmail.com>
2011-11-01 02:01:03 -0400

    Added round3. Layout changed.

--- src/ca/ajwest/irishpoker/IrishPokerActivity.java
+++ src/ca/ajwest/irishpoker/IrishPokerActivity.java
@@ -20,7 +20,7 @@ public class IrishPokerActivity extends Activity {
 	List<Integer> pickedCardList = new ArrayList<Integer>();
 	public int imageArr[] = new int[53]; //52 cards plus the back of a card
 	public TextView mTextView;
-	public Button mBlackButton, mRedButton, mNextButton;
+	public Button mBlackButton, mRedButton, mNextButton, mInsideButton, mOutsideButton;
 	public ImageButton mFirstPickedCard, mSecondPickedCard, mThirdPickedCard, mFourthPickedCard, mHigherButton1, mHigherButton2, mMainCard, mLeftCard, mRightCard;
 	public Card card1, card2, card3, card4;
 	
@@ -92,7 +92,7 @@ public class IrishPokerActivity extends Activity {
        
         mMainCard = (ImageButton) findViewById(R.id.buttonMainCard);
         mMainCard.setImageResource(R.drawable.back);
-        mFirstPickedCard = (ImageButton) findViewById(R.id.imageButton1); //TODO refactor the imageView names into imageButtons, because that's what they are.
+        mFirstPickedCard = (ImageButton) findViewById(R.id.imageButton1); 
         mSecondPickedCard = (ImageButton) findViewById(R.id.imageButton2);
         mThirdPickedCard = (ImageButton) findViewById(R.id.imageButton3);
         mFourthPickedCard = (ImageButton) findViewById(R.id.imageButton4);
@@ -118,7 +118,7 @@ public class IrishPokerActivity extends Activity {
 		mBlackButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
 
-				Log.i(LOG, "User thinks card is black. Generating card.");
+				Log.i(LOG, "User thinks card is black.");
 				//Generate a card
 		        Card current = generateCard();
 		        card1 = current;
@@ -187,8 +187,9 @@ public class IrishPokerActivity extends Activity {
     	mNextButton.setOnClickListener(new OnClickListener() {
     		public void onClick(View arg0) {
     			mSecondPickedCard.setImageResource(imageArr[card2.cardNum]);
+    			mSecondPickedCard.setVisibility(View.VISIBLE);
     			mMainCard.setImageResource(imageArr[0]);
-   			mNextButton.setVisibility(View.GONE);
+    			mNextButton.setVisibility(View.GONE);
     			Log.i(LOG, "Round3");
     			round3();
     		}           
@@ -204,7 +205,7 @@ public class IrishPokerActivity extends Activity {
     	
     	mHigherButton1.setOnClickListener(new OnClickListener() {
     		public void onClick(View arg0) {
-    			Log.i(LOG, "User thinks invis card is higher. Generating card.");
+    			Log.i(LOG, "User thinks invis card is higher.");
 				//Generate a card
 		        Card current = generateCard();
 		        card2 = current;
@@ -231,7 +232,7 @@ public class IrishPokerActivity extends Activity {
     	
     	mHigherButton2.setOnClickListener(new OnClickListener() {
     		public void onClick(View arg0) {
-    			Log.i(LOG, "User thinks vis card is higher. Generating card.");
+    			Log.i(LOG, "User thinks vis card is higher.");
 				//Generate a card
 		        Card current = generateCard();
 		        card2 = current;
@@ -253,12 +254,65 @@ public class IrishPokerActivity extends Activity {
     	});
     }
     
-    private void round3(){
+    int cardExplain1, cardExplain2;
+    private void round3(){ //is the next card going to be inside or outside of the range of the previous two cards?
+    	//Order the cards for easiser explain text and calculation below that
+    	
+    	if (card1.returnValue() < card2.returnValue()){
+    		cardExplain1 = card1.returnValue();
+    		cardExplain2 = card2.returnValue();
+    	}else{
+    		cardExplain1 = card2.returnValue();
+    		cardExplain2 = card1.returnValue();
+    	}
+    	
+    	mTextView.setText("Do you think the next card will be on the inside, or the outside of the range of these two cards? If you think the card will be in between " + cardExplain1 + " and " + cardExplain2 + " then select INSIDE. If you think it will be less than " + cardExplain1 + " or greater than " + cardExplain2 + " then select OUTSIDE.");
+  
+    	mInsideButton = (Button) findViewById(R.id.blackButton); 
+    	mInsideButton.setVisibility(View.VISIBLE);
+    	mInsideButton.setText("INSIDE");
+    	mInsideButton.setOnClickListener(new OnClickListener() {
+    		public void onClick(View arg0) {
+    			Log.i(LOG, "User has selected INSIDE.");
+    			Card current = generateCard();
+    			card3 = current;
+    			if ((current.returnValue()<cardExplain1)|(current.returnValue()>cardExplain2)){
+    				Log.i(LOG, "User is incorrect. " + current.returnValue() + " is less than " + cardExplain1 + " or greater than " + cardExplain2 + ".");
+    				mTextView.setText("You were incorrect. You must drink for " + current.returnValue() + " seconds.");
+    			}else{
+    				Log.i(LOG, "User is correct. " + current.returnValue() + " is more than " + cardExplain1 + " and less than " + cardExplain2 + ".");
+		        	mTextView.setText("You were correct. Make somebody else drink for " + current.returnValue() + " seconds.");
+    			}
+    		}           
+    	});
+
+    	mOutsideButton = (Button) findViewById(R.id.redButton); 
+    	mOutsideButton.setVisibility(View.VISIBLE);
+    	mOutsideButton.setText("OUTSIDE");
+    	mOutsideButton.setOnClickListener(new OnClickListener() {
+    		public void onClick(View arg0) {
+    			Log.i(LOG, "User has selected OUTSIDE.");
+    			Card current = generateCard();
+    			card3 = current;
+    			//flip the card over
+    			mMainCard.setImageResource(imageArr[current.cardNum]);
+    			if ((current.returnValue()<cardExplain1)|(current.returnValue()>cardExplain2)){
+    				Log.i(LOG, "User is correct. " + current.returnValue() + " is more than " + cardExplain1 + " or less than " + cardExplain2 + ".");
+    				mTextView.setText("You were correct. Make somebody else drink for " + current.returnValue() + " seconds.");
+    			}else{
+    				Log.i(LOG, "User is incorrect. " + current.returnValue() + " is greater than " + cardExplain1 + " and less than " + cardExplain2 + ".");
+    				mTextView.setText("You were incorrect. You must drink for " + current.returnValue() + " seconds.");
+    			}
+    		}           
+		});
+
+    	
     	
     }
     
     //spits out a card and adds it to the pile of discards.
     private Card generateCard() {
+    	Log.i(LOG, "Generating card.");
 
     	int i = 0;
 
@@ -280,7 +334,7 @@ public class IrishPokerActivity extends Activity {
     			}else{
     				Log.i(LOG, "Card pick sucessful. (Card wasn't already picked.) Add new card to list.");
     				pickedCardList.add(c.cardNum);
-    				Log.i(LOG, "New Card is: " + c.cardNum);
+    				Log.i(LOG, "New Card ID is: " + c.cardNum);
     				i = 1;
     				return c;
     			}
